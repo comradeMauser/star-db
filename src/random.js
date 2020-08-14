4522c2cecc52ee13df9a2862e25c68f211bda8f5
comradeMauser <60297848+comradeMauser@users.noreply.github.com>
2020-08-14 19:11:17 +0300

    upd

--- src/random.js
+++ src/random.js
@@ -4,41 +4,49 @@ import "./random.css"
 
 class Random extends React.Component {
 
-    localService = new Service()
+    localService = new Service();
+
     state = {
+        id: 2, //temporary plug: 0 and 1 do not have pictures
         name: "name",
         climate: "climate",
         diameter: "diameter",
         population: "population",
         rotationPeriod: "rotationPeriod",
-    }
+    };
 
 
     upd() {
-        this.localService.getPlanet(4).then((planet) => {
+        const id = Math.floor(Math.random() * 60)
+        this.localService.getPlanet(id).then((planet) => {
             this.setState({
+                id,
                 name: planet.name,
                 climate: planet.climate,
                 diameter: planet.diameter,
                 population: planet.population,
                 rotationPeriod: planet.rotation_period,
-            })
+            });
+            console.log("upd()")
         })
-    }
+    };
+
 
     render() {
-        const {name, climate, diameter, population, rotationPeriod} = this.state
+        const {id, name, climate, diameter, population, rotationPeriod} = this.state
+
         return (
             <div className="random">
-                Random block(Planet)
-                <div className="d-flex">
-                    <img className="randomImage" src="https://starwars-visualguide.com/assets/img/planets/2.jpg" alt="planet"/>
-                    <li style={{listStyleType: "none"}}>
-                        <ul>name: {name}</ul>
-                        <ul>climate: {climate}</ul>
-                        <ul>diameter: {diameter}</ul>
-                        <ul>population: {population}</ul>
-                        <ul>rotation period: {rotationPeriod}</ul>
+                Random block(Planet id: {id})
+                <div className="d-flex justify-content-center">
+                    <img src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+                         alt="random"/>
+                    <li className="list-group">
+                        <ul className="list-group-item">name: {name}</ul>
+                        <ul className="list-group-item">climate: {climate}</ul>
+                        <ul className="list-group-item">diameter: {diameter}</ul>
+                        <ul className="list-group-item">population: {population}</ul>
+                        <ul className="list-group-item">rotation period: {rotationPeriod}</ul>
                     </li>
                 </div>
                 <button className="btn-block btn-outline-warning" onClick={() => {
